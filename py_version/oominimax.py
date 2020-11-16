61094ff70781da19257cf4b0ec7077e0adaccdb2
Defrim2001 <59639093+Defrim2001@users.noreply.github.com>
2020-11-15 18:21:49 -0700

    Second update

--- py_version/oominimax.py
+++ py_version/oominimax.py
@@ -17,7 +17,6 @@ Example:
 Defrim Binakaj
 CCID:  1621841
 """
-
 class game_class():
     def _init_(self):
         self.HUMAN = -1
@@ -86,47 +85,6 @@ class game_class():
                 print(f'| {symbol} |', end='')
             print('\n' + str_line)
 
-    
-class occurence(game_class):  
-    
-    def human_turn(self,c_choice, h_choice):
-        """
-        The Human plays choosing a valid move.
-        :param c_choice: computer's choice X or O
-        :param h_choice: human's choice X or O
-        :return:
-        """
-        depth = len(self.empty_cells(self.board))
-        if depth == 0 or self.game_over(self.board):
-            return
-
-        # Dictionary of valid moves
-        move = -1
-        moves = {
-            1: [0, 0], 2: [0, 1], 3: [0, 2],
-            4: [1, 0], 5: [1, 1], 6: [1, 2],
-            7: [2, 0], 8: [2, 1], 9: [2, 2],
-        }
-        
-        self.clean()
-        print(f'Human turn [{h_choice}]')
-        self.render(self.board, c_choice, h_choice)
-
-        while move < 1 or move > 9:
-            try:
-                move = int(input('Use numpad (1..9): '))
-                coord = moves[move]
-                can_move = self.set_move(coord[0], coord[1], self.HUMAN)
-
-                if not can_move:
-                    print('Bad move')
-                    move = -1
-            except (EOFError, KeyboardInterrupt):
-                print('Bye')
-                exit()
-            except (KeyError, ValueError):
-                print('Bad choice')
-
     def game_over(self,state):
             """
             This function test if the human or computer wins
@@ -186,39 +144,7 @@ class occurence(game_class):
         else:
             return False
 
-    def minimax(self,state, depth, player):
-        """
-        AI function that choice the best move
-        :param state: current state of the board
-        :param depth: node index in the tree (0 <= depth <= 9),
-        but never nine in this case (see iaturn() function)
-        :param player: an human or a computer
-        :return: a list with [the best row, best col, best score]
-        """
-        if player == self.COMP:
-            best = [-1, -1, -infinity]
-        else:
-            best = [-1, -1, +infinity]
 
-        if depth == 0 or self.game_over(state):
-            score = self.evaluate(state)
-            return [-1, -1, score]
-
-        for cell in self.empty_cells(state):
-            x, y = cell[0], cell[1]
-            state[x][y] = player
-            score = self.minimax(state, depth - 1, -player)
-            state[x][y] = 0
-            score[0], score[1] = x, y
-
-            if player == self.COMP:
-                if score[2] > best[2]:
-                    best = score  # max value
-            else:
-                if score[2] < best[2]:
-                    best = score  # min value
-
-        return best
     def set_move(self,x, y, player):
         """
         Set the move on board, if the coordinates are valid
